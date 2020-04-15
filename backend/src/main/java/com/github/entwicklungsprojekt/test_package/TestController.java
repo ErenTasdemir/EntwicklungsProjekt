b767736a46c8097105222e35a5fa5ade22419bd3
Eren Cem Tasdemir <eren.cem.tasdemir@hotmail.com>
2020-04-15 19:42:25 +0200

    dfhdshshg

--- backend/src/main/java/com/github/entwicklungsprojekt/test_package/TestController.java
+++ backend/src/main/java/com/github/entwicklungsprojekt/test_package/TestController.java
@@ -1,18 +1,21 @@
 package com.github.entwicklungsprojekt.test_package;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/testendpoint")
 public class TestController {
 
-    @GetMapping
-    public ResponseEntity<?> testEndpoint(){
+    @PostMapping(path = "post/{summand}")
+    public ResponseEntity<?> testEndpoint(@RequestParam(required = true) String zahl, @PathVariable(name = "summand") String summand){
 
-        TestClass testClass = new TestClass();
+        int zahlParsed = Integer.parseInt(zahl);
+        zahlParsed += Integer.parseInt(summand);
+        TestClass testClass = new TestClass(zahlParsed);
+        return ResponseEntity.ok(testClass);
     }
 
 }
