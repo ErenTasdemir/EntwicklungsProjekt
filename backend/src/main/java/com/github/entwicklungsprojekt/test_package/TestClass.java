f1dc33efefe8c537e44c1cd65fa8864d3ba2b9ce
EnisIsaGoek <enisisa_goek@windowslive.com>
2020-04-15 18:59:44 +0200

    keine Funktion

---
+++ backend/src/main/java/com/github/entwicklungsprojekt/test_package/TestClass.java
@@ -0,0 +1,19 @@
+package com.github.entwicklungsprojekt.test_package;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import net.minidev.json.JSONUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.w3c.dom.ls.LSOutput;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class TestClass {
+
+    int zahl;
+
+
+}
+
