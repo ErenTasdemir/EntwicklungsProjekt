44aa32693b59d20f9155fb61fba4349ea6bd1f44
tasdemir <Eren-Cem.Tasdemir@adesso.de>
2020-04-15 18:13:13 +0200

    initial commit for backend

---
+++ backend/src/main/java/com/github/entwicklungsprojekt/EntwicklungsprojektApplication.java
@@ -0,0 +1,13 @@
+package com.github.entwicklungsprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntwicklungsprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntwicklungsprojektApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/github/entwicklungsprojekt/EntwicklungsprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.entwicklungsprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntwicklungsprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
