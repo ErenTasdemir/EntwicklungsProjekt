d0b49ee9728cdcab9678a907206f2846f6f0093f
EnisIsaGoek <enisisa_goek@windowslive.com>
2020-04-17 20:31:49 +0200

    feat: add prototype table (has to be filled with real data)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import {AddComponent} from './add/add.component';
+import {SearchComponent} from './search/search.component';
+import { RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import appRoutes from './appRoutes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AddComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatTableModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
