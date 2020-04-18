72d6ff68ac1ed54dc7c0c7e92a491af599a22ffa
Eren Cem Tasdemir <eren.cem.tasdemir@hotmail.com>
2020-04-18 21:18:39 +0200

    feat: add WebMvc config to allow http requests

---
+++ backend/src/main/java/com/github/entwicklungsprojekt/configuration/WebConfig.java
@@ -0,0 +1,19 @@
+package com.github.entwicklungsprojekt.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // allow CORS on all paths
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "OPTIONS");
+    }
+}
