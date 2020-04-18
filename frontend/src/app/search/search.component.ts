a59c75b286194ced2acd2a99e4db39fb3689a115
EnisIsaGoek <enisisa_goek@windowslive.com>
2020-04-19 00:49:05 +0200

    feat: add radius search functionality without styling

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 import {combineLatest, Subject} from 'rxjs';
 import {Shop, ShopService} from '../_services/shop.service';
 import {ActivatedRoute} from '@angular/router';
@@ -13,7 +13,22 @@ import {takeUntil} from 'rxjs/operators';
 export class SearchComponent implements OnInit {
   shops: Shop[] = [];
 
+  sendSearch = new EventEmitter<string>();
+
+  searchedShops: Shop[] = [];
+
+  query = '';
+
   destroy$ = new Subject<void>();
+  radiusInput: number;
+
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
 
   constructor(private shopService: ShopService,
               private route: ActivatedRoute) {
@@ -24,6 +39,11 @@ export class SearchComponent implements OnInit {
     console.log('Vor dem Laden der Shops');
     this.loadShops();
     console.log('Nach dem Laden der Shops');
+    this.sendSearch.subscribe(query => {
+      this.query = query;
+      console.log(query);
+      this.shopService.searchShops(this.query);
+    });
   }
 
   loadShops(): void {
@@ -36,13 +56,35 @@ export class SearchComponent implements OnInit {
       });
   }
 
-  replaceUmlaute(str) {
+  replaceInUmlaute(str) {
     return str.replace('UE', 'Ü')
     .replace('AE', 'Ä')
     .replace('OE', 'Ö')
     .replace('ue', 'ü')
     .replace('ae', 'ä')
     .replace('oe', 'ö');
+  }
 
+  replaceFromUmlaute(str) {
+    return str.replace('Ü', 'UE')
+      .replace('Ä', 'AE')
+      .replace('Ö', 'OE')
+      .replace('ü', 'ue')
+      .replace('ä', 'ae')
+      .replace('ö', 'oe');
+  }
+
+
+  sendSearchRequest(query: HTMLInputElement, location: HTMLInputElement, radius: number) {
+    console.log(location.value);
+    if (radius == null) {
+      radius = 0;
+    }
+    this.shopService.searchShopsByRadius(this.replaceFromUmlaute(query.value).toLowerCase(),
+      this.replaceFromUmlaute(location.value).toLowerCase(), radius)
+      .subscribe(data => {
+      console.log(data);
+      this.shops = data;
+    });
   }
 }
