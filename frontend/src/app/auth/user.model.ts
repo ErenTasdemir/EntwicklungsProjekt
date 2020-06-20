0df2542e09185d2be124072f98301d304c44d8ef
Eren Cem Tasdemir <eren.cem.tasdemir@hotmail.com>
2020-06-20 22:52:22 +0200

    fix: add auto-logout

--- frontend/src/app/auth/user.model.ts
+++ frontend/src/app/auth/user.model.ts
@@ -1,4 +1,4 @@
 export class User {
 
-  constructor(public id: string, public username: string, public name: string, public lastname: string, public token: string){}
+  constructor(public id: string, public username: string, public name: string, public lastname: string, public token: string, public tokenExpirationDate: Date){}
 }
