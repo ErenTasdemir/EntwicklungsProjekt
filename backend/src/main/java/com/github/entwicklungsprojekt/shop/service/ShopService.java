a38553989fbb28ebf13771cc37384b3c28cfe929
ErenTasdemir <eren.cem.tasdemir@hotmail.com>
2020-08-21 08:31:03 +0200

    fix: update new locations for edited shops

--- backend/src/main/java/com/github/entwicklungsprojekt/shop/service/ShopService.java
+++ backend/src/main/java/com/github/entwicklungsprojekt/shop/service/ShopService.java
@@ -14,7 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -103,6 +103,7 @@ public class ShopService {
         shopToEdit.setShopName(newName);
         shopToEdit.setShopType(newType);
         shopToEdit.setShopLocation(newLocation);
+        shopToEdit.setLocations(Collections.emptySet());
         shopRepository.save(shopToEdit);
         openstreetmapConnectionService.setLatitudeAndLongitudeForGivenShop(shopToEdit);
 
